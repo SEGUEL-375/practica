366a92deaa6d178251247c27a859fd79c5342ac0
SEGUEL-375 <samuelseguel375@gmail.com>
2024-08-03 11:22:11 -0300

    Se añadio nueva funcion en Java

--- prueba-de-envios-archivos-con-rama-samuel/practica.java
+++ prueba-de-envios-archivos-con-rama-samuel/practica.java
@@ -2,12 +2,20 @@ public class practica {
 
     public static void main(String[] args) {
         int numero = 29; // Puedes cambiar este número para probar otros valores
+        int numero_2 = 30;
         boolean esPrimo = esNumeroPrimo(numero);
+        boolean esPrimo2 = esNumeroPrimo_Two(numero_2);
 
         if (esPrimo) {
             System.out.println(numero + " es un número primo.");
         } else {
-            System.out.println(numero + " no es un número primo.");
+            System.out.println(numero_2 + " no es un número primo.");
+        }
+
+        if (esPrimo2){
+            System.out.println(numero_2 + "es un numero primo");
+        } else{
+            System.out.println(numero_2 + "no es un numero pirmo");
         }
     }
 
@@ -28,4 +36,17 @@ public class practica {
         return true; // Si no encontramos divisores, el número es primo
 
     }
+
+    public static boolean esNumeroPrimo_Two(int numero_2){
+        if (numero_2 <2) {
+           return false;
+        }
+        for (int i =2; i <= Math.sqrt(numero_2); i++){
+            if (numero_2 % i ==0){
+            return false;
+            }
+        }
+        return true;
+    }
+ 
 }
